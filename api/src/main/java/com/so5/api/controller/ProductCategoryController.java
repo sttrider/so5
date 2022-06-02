d5b1bb71aefa4c4f4afc7f49a6c86ef60dff632c
João Vitor <stridermgbr@gmail.com>
2022-06-01 23:35:00 -0300

    adding swagger

--- api/src/main/java/com/so5/api/controller/ProductCategoryController.java
+++ api/src/main/java/com/so5/api/controller/ProductCategoryController.java
@@ -2,6 +2,12 @@ package com.so5.api.controller;
 
 import com.so5.api.entity.ProductCategory;
 import com.so5.api.service.ProductCategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +22,12 @@ public class ProductCategoryController {
 
     private final ProductCategoryService productCategoryService;
 
+    @Operation(summary = "Find a list of categories")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Categories found", content = {
+                    @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = ProductCategory.class)))
+            })
+    })
     @GetMapping("/")
     public List<ProductCategory> findAll() {
 
--- api/src/main/java/com/so5/api/controller/ProductController.java
+++ api/src/main/java/com/so5/api/controller/ProductController.java
@@ -5,6 +5,12 @@ import com.so5.api.service.CustomerService;
 import com.so5.api.service.ProductService;
 import com.so5.api.vo.ProductSaveVO;
 import com.so5.api.vo.SearchProductVO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -26,6 +32,10 @@ public class ProductController {
     private final ProductService productService;
     private final CustomerService customerService;
 
+    @Operation(summary = "Create product")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Product created")
+    })
     @PostMapping("/")
     @Secured({"ROLE_admin"})
     public ResponseEntity<Void> create(@RequestBody ProductSaveVO productSaveVO, UriComponentsBuilder uriComponentsBuilder) {
@@ -37,6 +47,12 @@ public class ProductController {
         return ResponseEntity.created(uriComponents.toUri()).build();
     }
 
+    @Operation(summary = "Find a list of all products")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Products found", content = {
+                    @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Product.class)))
+            })
+    })
     @PostMapping("/list")
     @Secured({"ROLE_admin"})
     public List<Product> list(@RequestBody SearchProductVO searchProductVO) {
@@ -44,6 +60,10 @@ public class ProductController {
         return productService.list(searchProductVO);
     }
 
+    @Operation(summary = "Update a product status")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Product status updated")
+    })
     @PutMapping("/{sku}/{enabled}")
     @ResponseStatus(HttpStatus.OK)
     @Secured({"ROLE_admin"})
@@ -51,6 +71,11 @@ public class ProductController {
         productService.changeStatus(sku, enabled);
     }
 
+    @Operation(summary = "Update a product")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Product updated"),
+            @ApiResponse(responseCode = "404", description = "Product not found")
+    })
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     @Secured({"ROLE_admin"})
@@ -60,6 +85,10 @@ public class ProductController {
 
     }
 
+    @Operation(summary = "Delete a product")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Deleted")
+    })
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     @Secured({"ROLE_admin"})
@@ -67,18 +96,37 @@ public class ProductController {
         productService.delete(id);
     }
 
+    @Operation(summary = "Get a product by sku")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Product found", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Product.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Product not found")
+    })
     @GetMapping("/{sku}")
     public Product findBySku(@PathVariable String sku) {
 
         return productService.findBySku(sku);
     }
 
+    @Operation(summary = "Find a list of all active products")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Products found", content = {
+                    @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Product.class)))
+            })
+    })
     @PostMapping("/search")
     public List<Product> search(@RequestBody SearchProductVO searchProductVO) {
 
         return productService.search(searchProductVO);
     }
 
+    @Operation(summary = "Do the purchase")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Purchased"),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+            @ApiResponse(responseCode = "500", description = "User credit card not found")
+    })
     @PostMapping("/purchase")
     @ResponseStatus(HttpStatus.OK)
     @Secured({"ROLE_customer"})
